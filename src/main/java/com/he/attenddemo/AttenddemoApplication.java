53ca0d25866bc5f42726c2b1bcffbf062854e6b1
heningm <heningm@yonyou.com>
2019-01-08 18:13:16 +0800

    new commit

---
+++ src/main/java/com/he/attenddemo/AttenddemoApplication.java
@@ -0,0 +1,14 @@
+package com.he.attenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttenddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttenddemoApplication.class, args);
+	}
+
+}
+
