1914cc37c254f1bb8ab50867a7a5edd1c7f0db70
heningm <heningm@yonyou.com>
2019-01-09 17:22:38 +0800

    添加界面

--- src/main/java/com/he/attenddemo/controller/HelloController.java
+++ src/main/java/com/he/attenddemo/controller/HelloController.java
@@ -1,16 +1,25 @@
 package com.he.attenddemo.controller;
 
+import com.he.attenddemo.dao.IUserDao;
+import com.he.attenddemo.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Slf4j
 public class HelloController {
+    @Autowired
+    private IUserDao userDao;
     @RequestMapping("/hi")
-    private String Hello(){
-        log.error("");
-        return "hello";
+    private ModelAndView Hello(){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("hello");
+        User user=userDao.loadById("1");
+        modelAndView.addObject(user);
+        return modelAndView;
     }
 }
